1158311ce4275174cdfb500decbf64355b00d412
Peter Lee <plee831@berkeley.edu>
2016-09-06 17:13:31 -0700

    sending from server to client socket with getsockname() and sendto().

--- projects/proj1_chat/client.py
+++ projects/proj1_chat/client.py
@@ -10,10 +10,6 @@ client_name = NAME_TAG + sys.argv[1]
 host = sys.argv[2]
 port = int(sys.argv[3])
 
-recv_socket = socket.socket()
-recv_socket.bind(('', port+1))
-recv_socket.listen(1)
-
 try:
     client_socket = client_sock.connect((host, port))
 except socket.error, exc:
@@ -21,13 +17,16 @@ except socket.error, exc:
     pass
 client_sock.send(client_name)
 while True:
-    ready_to_read, ready_to_write, in_error = select.select([recv_socket], [client_sock], [], 0)
+    ready_to_read, ready_to_write, in_error = select.select([client_sock], [client_sock], [], 0)
     for read_sock in ready_to_read:
+        print('READING')
         (new_sock, address) = read_sock.accept()
         Data = new_sock.recv(1024)
         # TODO something with the data
         print(Data)
     for write_sock in ready_to_write:
+        # Data = write_sock.recv(1024)
+        # print(Data)
         raw_message_to_send = raw_input()
         if len(raw_message_to_send) != 200:
             padded_message_to_send = raw_message_to_send.ljust(200, ' ')
--- projects/proj1_chat/server.py
+++ projects/proj1_chat/server.py
@@ -3,45 +3,38 @@ import sys
 import select
 
 READ_SOCKET_LIST = []
-WRITE_SOCKET_LIST = []
+# WRITE_SOCKET_LIST = []
 
 server_socket = socket.socket()
 port = int(sys.argv[1])
 server_socket.bind(('', port))
-server_socket.listen(5)
+server_socket.listen(20)
 READ_SOCKET_LIST.append(server_socket)
 # dictionary of all the names to respective channels
-name_to_remote = {}
+remote_to_name = {}
 name_to_channel = {}
-messages_to_name = {}
+name_to_messages = {}
 client_name = ''
 
 while True:
-    ready_to_read, ready_to_write, in_error = select.select(READ_SOCKET_LIST, WRITE_SOCKET_LIST, [], 0)
-    for read_sock in ready_to_read:
+    ready_to_read, ready_to_write, in_error = select.select(READ_SOCKET_LIST, READ_SOCKET_LIST, [], 0)
+    for sock in ready_to_read:
         # new connection to server
-        if read_sock == server_socket:
+        if sock == server_socket:
             (new_sock, address) = server_socket.accept()
             READ_SOCKET_LIST.append(new_sock)
-            write_sock = socket.socket()
-            print(new_sock.getpeername())
-            t_host = new_sock.getpeername()[0]
-            t_port = new_sock.getpeername()[1]
-            print(address)
-            w_sock = write_sock.connect((address[0], t_port+1))
-            WRITE_SOCKET_LIST.append(write_sock)
         else:
             # STDOUT Server Side
-            Data = new_sock.recv(1024)
+            Data = sock.recv(1024)
             if '$$$$' in Data:
                 client_name = Data[4:]
-                name_to_remote[client_name] = str(new_sock.getpeername())
-            messages_to_name[client_name] = Data
-    #output to CLIENTS
-    for wr_sock in ready_to_write:
-        if str(wr_sock.getpeername()) == client_name:
-            # TODO logic with message (parse)
-            wr_sock.send(Data)
+                remote_to_name[str(sock.getpeername())] = client_name
+                print(client_name)
+            else:
+                client_name = remote_to_name[str(sock.getpeername())]
+                name_to_messages[client_name] = Data
+                # This sends to specific socket's address, not remote
+                sock.sendto(Data, sock.getsockname())
 
 
 # The first message that the server receives
