243eb0c39344128cbdccb190ec3573ecf73627b8
Peter Lee <plee831@berkeley.edu>
2016-11-02 13:23:41 -0700

    part1 tests pass for rtt-part-a and rtt-part-b

--- projects/proj3_measurement/rtts.py
+++ projects/proj3_measurement/rtts.py
@@ -6,6 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plot
 from matplotlib.backends import backend_pdf
 
+# "youtube.com": [": [14.475,
+
 
 def run_ping(hostnames, num_packets, raw_ping_output_filename, aggregated_ping_output_filename):
     PATTERN_PACKET_LOSS_PER = re.compile("\d+ packets transmitted, \d+ packets received, (\d+.\d+)% packet loss")
@@ -19,7 +21,8 @@ def run_ping(hostnames, num_packets, raw_ping_output_filename, aggregated_ping_o
     f2.write('{')
 
     for i in range(0, len(hostnames)):
-        f1.write("\"" + hostnames[i] + "\": [")
+        print hostnames[i]
+        f1.write("\""+hostnames[i]+"\": [")
         ping = subprocess.Popen(
             ["ping", "-c", str(num_packets), hostnames[i]],
             stdout=subprocess.PIPE,
@@ -40,7 +43,7 @@ def run_ping(hostnames, num_packets, raw_ping_output_filename, aggregated_ping_o
                 ping_info = PATTERN_PACKET_LOSS_PER.search(line).groups()
                 f2.write("\"" + hostnames[i] + "\": {\"drop_rate\": " + ping_info[0] + ", ")
                 if float(ping_info[0]) == 100.0:
-                    f2.write("\"max_rtt\": 0.0, \"median_rtt\": 0.0}")
+                    f2.write("\"max_rtt\": "+str(float(0.0))+", \"median_rtt\": +"+str(float(0.0))+"}")
                     if not i == len(hostnames) - 1:
                         f2.write(", ")
             elif PATTERN_TOTAL_RTT.match(line):
@@ -52,14 +55,14 @@ def run_ping(hostnames, num_packets, raw_ping_output_filename, aggregated_ping_o
                 dropped_packet = PATTERN_DROPED_PACKET.search(line).groups()[0]
                 last_seen = dropped_packet
                 if not dropped_packet == int(num_packets) - 1:
-                    f1.write("-1, ")
+                    f1.write("-1.0, ")
                 else:
-                    f1.write("-1")
+                    f1.write("-1.0")
                     done_with_ttls = True
             else:
                 if not done_with_ttls:
                     if int(last_seen) == int(num_packets) - 2:
-                        f1.write("-1")
+                        f1.write("-1.0")
                         done_with_ttls = True
         if i == len(hostnames) - 1:
             f1.write("]")
@@ -125,3 +128,24 @@ def plot_ping_cdf(raw_ping_results_filename, output_cdf_filename):
     my_file_path = os.path.abspath(output_cdf_filename)
     with backend_pdf.PdfPages(my_file_path) as pdf:
         pdf.savefig()
+
+if __name__ == "__main__":
+    f = open("alexa_top_100", "r")
+    alexa = [x for x in f.read().split("\n")]
+    alexa.remove("")
+    # print ("part a starting")
+    # run_ping(alexa, '10', 'rtt_a_raw.json', 'rtt_a_agg.json')
+    print ("part b starting")
+    run_ping(["google.com", "todayhumor.co.kr", "zanvarsity.ac.tz", "taobao.com"], '500', 'rtt_b_raw.json', 'rtt_b_agg.json')
+
+# sites_a = ["google.com", "facebook.com", "www.berkeley.edu", "allspice.lcs.mit.edu", "todayhumor.co.kr", "www.city.kobe.lg.jp", "www.vutbr.cz", "zanvarsity.ac.tz"]
+# sites_b = ["tpr-route-server.saix.net", "route-server.ip-plus.net", "route-views.oregon-ix.net", "route-server.eastern.allstream.com"]
+# run_traceroute(hostnames, 1, "test.txt")
+# run_traceroute(sites_a, 3, "tr_part_a.txt")
+# run_traceroute(sites_b, 3, "tr_part_b.txt")
+
+# parse_traceroute("test.txt", "test.json")
+# parse_traceroute("tr_part_a.txt", "tr_part_a.json")
+# parse_traceroute("tr_part_b.txt", "tr_part_b.json")
+# python project3_tests.py
+# python project3_tests.py --tr-part-b "tr_part_b.json"
